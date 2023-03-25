afeb96cfcad5da245ed9717bbc0ff9b952db05d0
Mayan Kumar Mishra <91338576+mishramayan@users.noreply.github.com>
2023-03-25 16:53:25 -0400

    Product lifecycle microservices based Application

---
+++ microservices-parent/product-service/src/test/java/com/product/productservice/service/ProductServiceTest.java
@@ -0,0 +1,17 @@
+package com.product.productservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+
+    @Test
+    void creatProduct() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+}
